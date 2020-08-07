fe5579a026d609f77f9273f717cce897f3a18488
SALIMONU AYODELE KABIR <39867606+ayodelesalimon@users.noreply.github.com>
2020-08-07 02:34:17 +0100

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/measurement_converter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.measurement_converter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Measurement Converter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextStyle labelStyle = TextStyle(
+    fontSize: 24,
+    color: Colors.grey[700],
+  );
+  final TextStyle inputStyle = TextStyle(
+    fontSize: 20,
+    color: Colors.blue[900],
+  );
+  String _startMeasure;
+  String _convertedMeasure;
+  final Map<String, int> _measuresMap = {
+    'meters': 0,
+    'kilometers': 1,
+    'grams': 2,
+    'kilograms': 3,
+    'feet': 4,
+    'miles': 5,
+    'pounds (lbs)': 6,
+    'ounces': 7,
+  };
+  final List<String> _measures = [
+    'meters',
+    'kilometers',
+    'grams',
+    'kilograms',
+    'feet',
+    'miles',
+    'pounds (lbs)',
+    'ounces',
+  ];
+  final dynamic _formulas = {
+    '0': [1, 0.001, 0, 0, 3.28084, 0.000621371, 0, 0],
+    '1': [1000, 1, 0, 0, 3280.84, 0.621371, 0, 0],
+    '2': [0, 0, 1, 0.0001, 0, 0, 0.00220462, 0.035274],
+    '3': [0, 0, 1000, 1, 0, 0, 2.20462, 35.274],
+    '4': [0.3048, 0.0003048, 0, 0, 1, 0.000189394, 0, 0],
+    '5': [1609.34, 1.60934, 0, 0, 5280, 1, 0, 0],
+    '6': [0, 0, 453.592, 0.453592, 0, 0, 1, 16],
+    '7': [0, 0, 28.3495, 0.0283495, 3.28084, 0, 0.0625, 1],
+  };
+  double _numberFrom;
+  String _resultMessage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Value',
+              style: labelStyle,
+            ),
+            TextField(
+              style: inputStyle,
+              decoration: InputDecoration(
+                
+                hintText: "Please insert the measure to be converted",
+              ),
+              onChanged: (text) {
+                var rv = double.tryParse(text);
+                if (rv != null) {
+                  setState(() {
+                    _numberFrom = rv;
+                  });
+                }
+              },
+            ),
+            Text(
+              'From',
+              style: labelStyle,
+            ),
+            DropdownButton(
+              isExpanded: true,
+              style: inputStyle,
+              items: _measures.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  _startMeasure = value;
+                });
+              },
+              value: _startMeasure,
+            ),
+            Text(
+              'To',
+              style: labelStyle,
+            ),
+            DropdownButton(
+              isExpanded: true,
+              style: inputStyle,
+              items: _measures.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  _convertedMeasure = value;
+                });
+              },
+              value: _convertedMeasure,
+            ),
+            RaisedButton(
+              child: Text('Convert', style: inputStyle),
+              onPressed: () {
+                if (_startMeasure.isEmpty ||
+                    _convertedMeasure.isEmpty ||
+                    _numberFrom == 0) {
+                  return;
+                } else {
+                  convert(_numberFrom, _startMeasure, _convertedMeasure);
+                }
+              },
+            ),
+            Text((_resultMessage == null) ? '' : _resultMessage,
+                style: labelStyle),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void convert(double value, String from, String to) {
+    int nFrom = _measuresMap[from];
+    int nTo = _measuresMap[to];
+    var multiplier = _formulas[nFrom.toString()][nTo];
+    var result = value * multiplier;
+    if (result == 0) {
+      _resultMessage = 'This conversion cannot be performed';
+    } else {
+      _resultMessage =
+          '${_numberFrom.toString()} $_startMeasure are ${result.toString()} $_convertedMeasure';
+    }
+    setState(() {
+      _resultMessage = _resultMessage;
+    });
+  }
+}
